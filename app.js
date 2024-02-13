30034f847a960ff47b8030844c9a8a4d2dadc45e
waarrdii <waarrdii@gmail.com>
2024-02-13 12:08:54 +0700

    Cart

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+  el: '#app',
+  data : {
+    maximum: 50,
+    products: null,
+    cart: []
+  },
+  mounted:function(){
+    fetch('https://hplussport.com/api/products/order/id')
+    .then (response=>response.json())
+    .then (data =>{
+      this.products = data;
+    })
+  },
+  methods:{
+    addItem:function(product){
+      this.cart.push(product);
+    }
+  }
+});
+
+
